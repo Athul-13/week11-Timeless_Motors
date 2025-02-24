e066d50cef2a338bf111837f40c7720c7a9f2885
Athul-13 <kkunnikrishnan13@gmail.com>
2025-02-24 18:52:01 +0530

    maybe last1

--- server/socket/socketConfig.js
+++ server/socket/socketConfig.js
@@ -8,7 +8,7 @@ const {Server} = require('socket.io');
 const initializeSocket = (server) => {
     const io = new Server(server, {
         cors: {
-            origin: 'http://localhost:5173',
+            origin: ["http://localhost:5173", "https://week11-timeless-motors.vercel.app"],
             methods: ['GET', 'POST'],
             credentials: true
         }
